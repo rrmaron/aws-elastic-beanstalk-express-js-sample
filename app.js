f1565b8280beb84488ab249f2eb7fa175d4f8f04
Roger Maron <rrmaron@yahoo.com>
2021-02-13 03:52:55 -0600

    Full pipeline test 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('What an Amazing Beautiful World!'));
+app.get('/', (req, res) => res.send('Hee haw!!  What an Amazing Beautiful World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
